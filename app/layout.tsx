ca2c99f19c7f416b5b66c020474ff745f30f8fc2
vbrnbs <varszegibarnabas@gmail.com>
2024-09-10 21:00:59 +1200

    theme added and theme toggler

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
+import { ModeToggle } from '../components/ui/ModeToggle';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,11 +26,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ModeToggle />
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { client } from '../sanity/lib/client';
 import { groq } from 'next-sanity';
+import { Button } from "@/components/ui/button"
 
 const blogQuery = groq`
   *[_type == "post"]{
@@ -16,8 +17,10 @@ export default async function Home() {
   const posts = response;
 
   return (
-    <div className='text-white'>
+<div className="bg-background text-foreground" >
+
       blog
+      
       {posts.map((post: any) => (
         <div key={post._id}>
           <h2>{post.title}</h2>
@@ -25,6 +28,8 @@ export default async function Home() {
           {/* <p>{post.body}</p> */}
         </div>
       ))}
+      <Button variant="outline">Click me</Button>
     </div>
   );
 }
+
